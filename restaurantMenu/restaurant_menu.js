a11350b27d2dee3cf2c6d45f50c32bdb172cbb38
kai492 <143822409+kai492@users.noreply.github.com>
2024-03-17 00:13:06 +0800

    Update restaurant_menu.js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -5,6 +5,16 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
+//The provided code converts breakfast menu array items into HTML strings using map() and an arrow function to
+//structure each item's HTML format.
+
+//Subsequently, a string concatenation method has been used to join the generated HTML strings into one cohesive string
+  //  using join ('') to prepare for insertion.
+
+//Finally, this concatenated HTML is dynamically updated within the specific HTML element identified by
+//breakfastMenuItems ID with the concatenated HTML string, dynamically populating the webpage with the breakfast
+   // menu items in formatted paragraphs <p> tags.
+
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
